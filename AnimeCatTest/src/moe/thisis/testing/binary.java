e60462613e70bf8f7e1d146baefcb8fb37881539
BuildTools <unconfigured@null.spigotmc.org>
2016-11-11 18:09:18 -0500

    int to binary

---
+++ AnimeCatTest/src/moe/thisis/testing/binary.java
@@ -0,0 +1,18 @@
+package moe.thisis.testing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class binary {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		int i = in.read();
+		for (int x=0;x<i;x++){
+			
+		}
+	}
+
+}
